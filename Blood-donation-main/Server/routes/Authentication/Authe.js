c4a4ccad2044adf10675b68eaa3cc098c623bfcd
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-02-19 15:01:33 +0530

    added

--- Blood-donation-main/Server/routes/Authentication/Authe.js
+++ Blood-donation-main/Server/routes/Authentication/Authe.js
@@ -2,9 +2,12 @@ import express from "express";
 import Signupuser from "../../controllers/Signup.js";
 import verifyOtp from "../../controllers/VerifyOtp.js";
 import Login from "../../controllers/Login.js";
+import Logout from "../../controllers/Logout.js";
+import verifyJwt from "../../middleware/VerifyJwt.js";
 const router = express.Router();
 router.post("/signup", Signupuser);
 router.post("/verifyotp", verifyOtp);
-router.post("/login", Login)
+router.post("/login", verifyJwt, Login)
+router.get("/logout", Logout)
 
 export default router;
